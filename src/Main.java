a25d13970857f6b0cd13732323f74fd626b5e2a7
Ksenija <omega-2077@list.ru>
2024-06-30 20:52:23 +0300

    change task2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int ammount = 15680;
+        int mile = 20;
+
+
+        System.out.println("итоговый бонус:" + (ammount / mile));
     }
 }
